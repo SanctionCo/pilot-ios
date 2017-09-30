4ebb09cdffa6f78af99ff7705fcf8365e4da7d37
Nick Eckert <n.eckert70@gmail.com>
2017-09-30 01:20:20 -0500

    Fixed Table overlap bug (#26)
    * Fixed Table overlap bug

--- pilot/ProfileViewController.swift
+++ pilot/ProfileViewController.swift
@@ -2,18 +2,21 @@
 //  ProfileViewController.swift
 //  pilot
 //
-//  Created by Nick Eckert on 9/26/17.
+//  Created by Nick Eckert on 9/29/17.
 //  Copyright © 2017 sanction. All rights reserved.
 //
 
 import UIKit
 
-class ProfileViewController: UIViewController {
-
+class ProfileViewController: UITableViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        navigationItem.largeTitleDisplayMode = .never
+        self.navigationItem.largeTitleDisplayMode = .never
+        
+        let customHeaderView = Bundle.main.loadNibNamed("CustomTableHeaderView", owner: self, options: nil)?.first as! UIView
+        self.tableView.tableHeaderView = customHeaderView
     }
-
+    
 }
