78e4b30de9aae871fdfab6595e3beb8ae2b18c19
Nick Eckert <n.eckert70@gmail.com>
2017-09-30 02:16:41 -0500

    Renamed unused closure parameter

--- pilot/CreateViewController.swift
+++ pilot/CreateViewController.swift
@@ -48,7 +48,7 @@ class CreateViewController: UIViewController {
             activitySpinner.startAnimating()
             signUpButton.isEnabled = false
             
-            PilotUser.upload(with: ThunderRouter.createPilotUser(pilotUser), onSuccess: { pilotUser in
+            PilotUser.upload(with: ThunderRouter.createPilotUser(pilotUser), onSuccess: { _ in
                 
                 DispatchQueue.main.async { [weak self] in
                     self?.activitySpinner.stopAnimating()
